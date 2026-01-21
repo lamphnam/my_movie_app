35db98bc517e99ffc46d0ec983b159edde7bbcd7
lamphnam <nampham2131@gmail.com>
2026-01-21 16:56:56 +0700

    update feature content

--- src/config/featuredContent.ts
+++ src/config/featuredContent.ts
@@ -24,24 +24,24 @@ export const featuredMovies: MovieListItem[] = [
     casts: "Đỗ Nhật Hoàng, Steven Nguyễn, Phương Nam",
   },
   {
-    name: "Mang Mẹ Đi Bỏ",
-    slug: "mang-me-di-bo",
-    original_name: "Leaving Mom",
+    name: "Tiếng Yêu Này, Anh Dịch Được Không?",
+    slug: "tieng-yeu-nay-anh-dich-duoc-khong",
+    original_name: "Can This Love Be Translated?",
     thumb_url:
-      "https://phim.nguonc.com/public/images/Film/5SiQsz5YCxZdCN5vhKdiQ2dQnr6.jpg",
+      "https://phim.nguonc.com/public/images/Post/7/tieng-yeu-nay-anh-dich-duoc-khong.jpg",
     poster_url:
-      "https://phim.nguonc.com/public/images/Film/uPexrwvz2LLJomJUCNoxim8tQWD.jpg",
-    created: "2025-11-30T08:02:16.000000Z",
-    modified: "2025-11-30T08:24:55.000000Z",
+      "https://phim.nguonc.com/public/images/Post/7/tieng-yeu-nay-anh-dich-duoc-khong-1.jpg",
+    created: "2026-01-16T12:28:27.000000Z",
+    modified: "2026-01-16T13:07:14.000000Z",
     description:
-      'Rời Xa Mẹ kể về câu chuyện của Hoan (Tuấn Trần), một chàng trai trẻ kiếm sống bằng nghề cắt tóc "gã hề đường phố" để chăm sóc mẹ. Mẹ anh, Lê Thị Hạnh (Hồng Đào), mắc bệnh Alzheimer và cư xử như một đứa trẻ. Căn bệnh của bà không chỉ là gánh nặng tài chính mà còn tước đi cuộc sống tự do và những ước mơ mà anh vẫn đang theo đuổi. Một ngày nọ, Hoan đưa ra một quyết định định mệnh - "gửi mẹ" cho anh trai mình ở Hàn Quốc, một người mà anh thậm chí chưa từng gặp mặt.',
-    total_episodes: 1,
-    current_episode: "FULL",
-    time: "112 phút",
+      "Đi khắp thế giới để quay chương trình truyền hình, cảm xúc của một người nổi tiếng và phiên dịch viên của cô lại chẳng thể nào thông dịch. Liệu tình yêu có tìm được ngôn ngữ riêng?",
+    total_episodes: 12,
+    current_episode: "Hoàn tất (12/12)",
+    time: "65 phút/tập",
     quality: "HD",
-    language: "Vietsub",
-    director: "Mo Hong-jin",
-    casts: "Tuấn Trần, Hồng Đào, Juliet Bảo Ngọc Doling",
+    language: "Vietsub + Lồng Tiếng",
+    director: "Hong Mi-ran, Hong Jeong-eun",
+    casts: "Kim Seon Ho, Go Youn Jung, Fukushi Sota",
   },
   {
     name: "Thám Tử Kiên: Kỳ Án Không Đầu",
@@ -62,46 +62,5 @@ export const featuredMovies: MovieListItem[] = [
     language: "Vietsub",
     director: "Victor Vũ",
     casts: "Quốc Huy, Ngọc Diệp, Minh Anh",
-  },
-  {
-    name: "Hãy Cưới Em Nhé?",
-    slug: "hay-cuoi-em-nhe",
-    original_name: "Would You Marry Me?",
-    thumb_url:
-      "https://phim.nguonc.com/public/images/Post/6/anh-co-muon-cuoi-em-khong.jpg",
-    poster_url:
-      "https://phim.nguonc.com/public/images/Post/6/anh-co-muon-cuoi-em-khong-1.jpg",
-    created: "2025-10-10T15:47:35.000000Z",
-    modified: "2025-10-19T05:41:49.000000Z",
-    description:
-      'Yoo Me-ri (Jung So-min thủ vai) – một nhà thiết kế nội thất đang gặp khó khăn về tài chính và tình cảm – bất ngờ trúng giải trong một chương trình tặng nhà cho các cặp đôi mới cưới. Tuy nhiên, trớ trêu thay, cô vừa mới hủy hôn với bạn trai cũ sau khi phát hiện anh ta ngoại tình. Để không bỏ lỡ cơ hội có được căn hộ mơ ước, Me-ri quyết định giả vờ kết hôn. Cô tình cờ gặp Kim Woo-joo (Choi Woo-shik thủ vai) – một nhân viên marketing, người thừa kế đời thứ tư của một tiệm bánh truyền thống danh tiếng – và đề nghị anh cùng "kết hôn giả" trong vòng 90 ngày.',
-    total_episodes: 12,
-    current_episode: "Tập 4",
-    time: "70 phút/tập",
-    quality: "HD",
-    language: "Vietsub + Thuyết Minh",
-    director: "Hwang In Hyeok, Song Hyun Wook",
-    casts:
-      "Bae Na Ra, Choi Woo Shik, Heo Ji Na, Jung So Min, Seo Bum June, Shin Seul Ki",
-  },
-  {
-    name: "Thần đèn ơi, ước đi",
-    slug: "than-den-oi-uoc-di",
-    original_name: "Genie, Make a Wish",
-    thumb_url:
-      "https://phim.nguonc.com/public/images/Post/10/than-den-oi-uoc-di.jpg",
-    poster_url:
-      "https://phim.nguonc.com/public/images/Post/10/than-den-oi-uoc-di-1.jpg",
-    created: "2025-10-03T08:05:02.000000Z",
-    modified: "2025-10-03T08:47:00.000000Z",
-    description:
-      "Sau một ngàn năm, vị thần đèn kỳ quặc nọ trở lại để ban điều ước cho người phụ nữ bẩm sinh vô cảm. Liệu phép thuật của anh có thể mang tình yêu và điều kỳ diệu đến thế giới của cô?",
-    total_episodes: 13,
-    current_episode: "Hoàn tất (13/13)",
-    time: "60 phút/tập",
-    quality: "HD",
-    language: "Vietsub + Lồng Tiếng",
-    director: "Lee Byeong-Hun, Ahn Gil-Ho",
-    casts: "Kim Woo Bin, Suzy, Kim Me Kyung",
   },
 ];
